a1f2e834b4069431eb1a307ed93cf40dabb88b73
Hugo <HugoDF@users.noreply.github.com>
2020-09-03 20:11:23 +0100

    feat(cache-dir-in-node_modules) (#7)
    * ava -> uvu
    
    * use find-cache-dir

--- tests/inline-js.js
+++ tests/inline-js.js
@@ -1,4 +1,5 @@
-const test = require('ava').default;
+const {test} = require('uvu');
+const assert = require('uvu/assert');
 const {inlineJs} = require('../src/main');
 
 const defaultOptions = {
@@ -7,8 +8,8 @@ const defaultOptions = {
 
 const remoteInclude = `<script src="https://cdn.jsdelivr.net/npm/@shat/stylenames@v1.x.x/lib/index.umd.js"></script>`;
 
-test('inlines modules smaller than passed "maxSize" option', async (t) => {
-  t.is(
+test('inlines modules smaller than passed "maxSize" option', async () => {
+  assert.fixture(
     await inlineJs(remoteInclude, {maxSize: 20000}),
     `<script>!function(e,t){"object"==typeof exports&&"undefined"!=typeof module?module.exports=t():"function"==typeof define&&define.amd?define(t):(e=e||self).stylenames=t()}(this,function(){function e(e){if(!e||"object"!=typeof e)return"";if(Array.isArray(e))return e.join(";")+";";for(var t="",o=0,n=Object.keys(e);o<n.length;o++){var f=n[o],r=e[f];if("string"!=typeof r)if("boolean"!=typeof r){if("object"==typeof r&&0!==r.length)for(var i=e[f],s=0,a=Object.keys(i);s<a.length;s++){var y=a[s];if("function"!=typeof i[y]&&i[y]||"function"==typeof i[y]&&i[y]()){t+=f+":"+y+";";break}}}else t+=f;else t+=f+":"+r+";"}return t.replace(/([a-z])([A-Z])/g,"$1-$2").replace(/[_\\s]/,"-").toLowerCase()}return window&&(window.styleNames=e),e});
 //# sourceMappingURL=index.umd.js.map
@@ -16,14 +17,14 @@ test('inlines modules smaller than passed "maxSize" option', async (t) => {
   );
 });
 
-test('keeps CDN tag for modules larger than "maxSize" option', async (t) => {
-  t.is(await inlineJs(remoteInclude, {maxSize: 10}), remoteInclude);
+test('keeps CDN tag for modules larger than "maxSize" option', async () => {
+  assert.fixture(await inlineJs(remoteInclude, {maxSize: 10}), remoteInclude);
 });
 
 const relativeScriptInclude = `<script src="./reflect.js"></script>`;
 
-test('keeps relative includes', async (t) => {
-  t.is(
+test('keeps relative includes', async () => {
+  assert.is(
     await inlineJs(relativeScriptInclude, defaultOptions),
     relativeScriptInclude
   );
@@ -31,6 +32,11 @@ test('keeps relative includes', async (t) => {
 
 const sampleInlineScript = `<script>console.log('hello world');</script>`;
 
-test('keeps inline scripts', async (t) => {
-  t.is(await inlineJs(sampleInlineScript, defaultOptions), sampleInlineScript);
+test('keeps inline scripts', async () => {
+  assert.is(
+    await inlineJs(sampleInlineScript, defaultOptions),
+    sampleInlineScript
+  );
 });
+
+test.run();
--- tests/main.js
+++ tests/main.js
@@ -1,11 +1,12 @@
-const test = require('ava').default;
+const {test} = require('uvu');
+const assert = require('uvu/assert');
 const fs = require('fs').promises;
 const path = require('path');
 const {ensureWriteablePath} = require('../src/utils');
 const {main} = require('../src/main');
 
 const sampleHtml = `<link
-  href="https://cdn.jsdelivr.net/npm/tailwindcss@1.x.x/dist/tailwind.min.css"
+  href="https://cdn.jsdelivr.net/npm/tailwindcss@1.7.x/dist/tailwind.min.css"
   rel="stylesheet"
 />`;
 
@@ -13,7 +14,7 @@ test.before(async () => {
   await ensureWriteablePath('dist/index.html');
 });
 
-test('works with different output directory', async (t) => {
+test('works with different output directory', async () => {
   const dir = await fs.mkdtemp('dist/origin-dir');
   const outDir = await fs.mkdtemp('dist/output-dir');
   await fs.writeFile(path.join(dir, 'test.html'), sampleHtml, 'utf8');
@@ -21,22 +22,24 @@ test('works with different output directory', async (t) => {
   const original = await fs.readFile(path.join(dir, 'test.html'), 'utf8');
   const output = await fs.readFile(path.join(outDir, 'test.html'), 'utf8');
 
-  t.is(original, sampleHtml);
-  t.is(
+  assert.is(original, sampleHtml);
+  assert.is(
     output,
-    '<style>/*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */*,::after,::before{box-sizing:border-box;border-width:0;border-style:solid;border-color:#e2e8f0}</style>'
+    '<style>/*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */*,::after,::before{box-sizing:border-box;border-width:0;border-style:solid;border-color:#e2e8f0}@keyframes spin{to{transform:rotate(360deg)}}@keyframes ping{100%,75%{transform:scale(2);opacity:0}}@keyframes pulse{50%{opacity:.5}}@keyframes bounce{0%,100%{transform:translateY(-25%);animation-timing-function:cubic-bezier(.8,0,1,1)}50%{transform:none;animation-timing-function:cubic-bezier(0,0,.2,1)}}</style>'
   );
 });
 
-test('works when writing in-place', async (t) => {
+test('works when writing in-place', async () => {
   const dir = await fs.mkdtemp('dist/in-place');
 
   await fs.writeFile(path.join(dir, 'test.html'), sampleHtml, 'utf8');
   await main(`${dir}/*.html`, {maxSize: 20000});
   const original = await fs.readFile(path.join(dir, 'test.html'), 'utf8');
 
-  t.is(
+  assert.is(
     original,
-    '<style>/*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */*,::after,::before{box-sizing:border-box;border-width:0;border-style:solid;border-color:#e2e8f0}</style>'
+    '<style>/*! normalize.css v8.0.1 | MIT License | github.com/necolas/normalize.css */*,::after,::before{box-sizing:border-box;border-width:0;border-style:solid;border-color:#e2e8f0}@keyframes spin{to{transform:rotate(360deg)}}@keyframes ping{100%,75%{transform:scale(2);opacity:0}}@keyframes pulse{50%{opacity:.5}}@keyframes bounce{0%,100%{transform:translateY(-25%);animation-timing-function:cubic-bezier(.8,0,1,1)}50%{transform:none;animation-timing-function:cubic-bezier(0,0,.2,1)}}</style>'
   );
 });
+
+test.run();
